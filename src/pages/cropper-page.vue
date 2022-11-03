c09b3cf86fac5cd1573d80c9762a3affdcf97353
weekkkk <nikitanedelko9@gmail.com>
2022-11-03 09:37:53 +0200

    clear cropper

--- src/pages/cropper-page.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <n-cropper></n-cropper>
-</template>
-
-<script lang="ts" setup>
-import NCropper from "@/components/cropper/n-cropper.vue";
-</script>
-
-<style lang="less" scoped></style>

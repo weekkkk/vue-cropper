07d620d5ab69add8967db024c5dab31a8953499d
weekkkk <nikitanedelko9@gmail.com>
2023-01-20 23:14:12 +0200

    update styles

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,9 @@
-import { createApp } from "vue";
-import App from "./App.vue";
-import router from "./router";
+import { createApp } from 'vue';
+import App from './App.vue';
+import router from './router';
 
-import './assets/style/index.scss'
+import './assets/style/index.scss';
 
 const app = createApp(App);
 
-app.use(router).mount("#app");
+app.use(router).mount('#app');
